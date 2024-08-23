1f18aee99f5332ac0c529f4473ddc13a4d976a28
Adrian Suliga <adriansuliga81@gmail.com>
2024-08-23 16:32:06 +0200

    Add ADC ex 3 from FORBOT STM32 course

--- ADC_ex_2/Core/Src/main.c
+++ ADC_ex_2/Core/Src/main.c
@@ -22,6 +22,7 @@
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 #include "stdio.h"
+#include "stdlib.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -41,6 +42,7 @@
 
 /* Private variables ---------------------------------------------------------*/
 ADC_HandleTypeDef hadc1;
+DMA_HandleTypeDef hdma_adc1;
 
 RTC_HandleTypeDef hrtc;
 
@@ -55,6 +57,7 @@ UART_HandleTypeDef huart2;
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
+static void MX_DMA_Init(void);
 static void MX_ADC1_Init(void);
 static void MX_USART2_UART_Init(void);
 static void MX_RTC_Init(void);
@@ -103,6 +106,7 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
+  MX_DMA_Init();
   MX_ADC1_Init();
   MX_USART2_UART_Init();
   MX_RTC_Init();
@@ -113,7 +117,12 @@ int main(void)
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
+  volatile static uint16_t adc_value[3];
+  uint16_t old_x = 0;
+  uint16_t old_y = 0;
+
   HAL_ADCEx_Calibration_Start(&hadc1);
+  HAL_ADC_Start_DMA(&hadc1, (uint32_t*)adc_value, 3);
 
   HAL_TIM_Base_Start_IT(&htim3);
   HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_1);
@@ -121,13 +130,24 @@ int main(void)
 
   while (1)
   {
-	  HAL_ADC_Start(&hadc1);
-	  HAL_ADC_PollForConversion(&hadc1, HAL_MAX_DELAY);
+	  uint16_t pulse = adc_value[0] / 400;
+	  pulse = 2000 - pulse * 500;
+	  if (pulse < 0)
+		  pulse = 0;
 
-	  uint32_t adc_value = HAL_ADC_GetValue(&hadc1) / 400;
-	  __HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_1, 2000 - adc_value * 500);
-	  __HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_2, 2000 - adc_value * 500);
-	  HAL_Delay(200);
+	  __HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_1, pulse);
+	  __HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_2, pulse);
+
+	  if (
+			  abs(old_x - adc_value[1]) > 100 ||
+			  abs(old_y - adc_value[2]) > 100
+	  )
+	  {
+		  printf("x = %d, y = %d\n", adc_value[1], adc_value[2]);
+		  old_x = adc_value[1];
+		  old_y = adc_value[2];
+	  }
+	  //HAL_Delay(200);
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
@@ -201,12 +221,12 @@ static void MX_ADC1_Init(void)
   /** Common config
   */
   hadc1.Instance = ADC1;
-  hadc1.Init.ScanConvMode = ADC_SCAN_DISABLE;
-  hadc1.Init.ContinuousConvMode = DISABLE;
+  hadc1.Init.ScanConvMode = ADC_SCAN_ENABLE;
+  hadc1.Init.ContinuousConvMode = ENABLE;
   hadc1.Init.DiscontinuousConvMode = DISABLE;
   hadc1.Init.ExternalTrigConv = ADC_SOFTWARE_START;
   hadc1.Init.DataAlign = ADC_DATAALIGN_RIGHT;
-  hadc1.Init.NbrOfConversion = 1;
+  hadc1.Init.NbrOfConversion = 3;
   if (HAL_ADC_Init(&hadc1) != HAL_OK)
   {
     Error_Handler();
@@ -216,7 +236,25 @@ static void MX_ADC1_Init(void)
   */
   sConfig.Channel = ADC_CHANNEL_0;
   sConfig.Rank = ADC_REGULAR_RANK_1;
-  sConfig.SamplingTime = ADC_SAMPLETIME_1CYCLE_5;
+  sConfig.SamplingTime = ADC_SAMPLETIME_239CYCLES_5;
+  if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Configure Regular Channel
+  */
+  sConfig.Channel = ADC_CHANNEL_4;
+  sConfig.Rank = ADC_REGULAR_RANK_2;
+  if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Configure Regular Channel
+  */
+  sConfig.Channel = ADC_CHANNEL_5;
+  sConfig.Rank = ADC_REGULAR_RANK_3;
   if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
   {
     Error_Handler();
@@ -351,6 +389,22 @@ static void MX_USART2_UART_Init(void)
   /* USER CODE BEGIN USART2_Init 2 */
 
   /* USER CODE END USART2_Init 2 */
+
+}
+
+/**
+  * Enable DMA controller clock
+  */
+static void MX_DMA_Init(void)
+{
+
+  /* DMA controller clock enable */
+  __HAL_RCC_DMA1_CLK_ENABLE();
+
+  /* DMA interrupt init */
+  /* DMA1_Channel1_IRQn interrupt configuration */
+  HAL_NVIC_SetPriority(DMA1_Channel1_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(DMA1_Channel1_IRQn);
 
 }
 
--- README.md
+++ README.md
@@ -4,3 +4,4 @@ Repository holds exercises and projects done on STM32 microcontroller
 # ADC exercise
 - Exercise 1: Use potentiometer to manipulate blinking frequency of 2 LEDs. 
 - Exercise 2: Use value from photoresistor to manipulate brightness of 2 LEDs.
+- Exercise 3: Add joystick to previous project. The board should read ADC value from this joystick and send them to PC via UART whenever user moves the stick.
